e893dcf3ec3edb4002a36c77663635c7880c44d2
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-01-16 23:42:30 -0500

    Fix postgres connection string

--- pkg/database/DBEngine.py
+++ pkg/database/DBEngine.py
@@ -6,7 +6,7 @@ class DBEngine:
     __db: Engine
 
     def __init__(self):
-        db_string = "postgres://" + os.getenv("POSTGRES_USER") + ":" + os.getenv("POSTGRES_PASSWORD") + "@" + os.getenv("POSTGRES_HOST") + ":" + os.getenv("POSTGRES_PORT") + "/" + os.getenv("POSTGRES_DB")
+        db_string = "postgresql://" + os.getenv("POSTGRES_USER") + ":" + os.getenv("POSTGRES_PASSWORD") + "@" + os.getenv("POSTGRES_HOST") + ":" + os.getenv("POSTGRES_PORT") + "/" + os.getenv("POSTGRES_DB")
         self.__db = create_engine(db_string)
 
     def get(self) -> Engine:
