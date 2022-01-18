61ed9de5eb43af80371c0d2a2d0d78d3dd9b9c5d
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-01-17 23:18:02 -0500

    Add cmd.main offline mode

--- cmd/main.py
+++ cmd/main.py
@@ -1,6 +1,8 @@
+import json
 from dotenv import load_dotenv
 from pkg.api.webengine import WebEngine, WebEngineConfig
 from pkg.api.ecomm.WwwGundamhobbyCa import WwwGundamhobbyCa
+from pkg.api.ecomm.Item import Item
 import os
 
 from pkg.database.DBEngine import DBEngine
@@ -13,12 +15,21 @@ def main():
     webEngineConfig = WebEngineConfig()
     webEngineConfig.headless = os.getenv("DEBUG").upper() != "TRUE"
 
-    webEngine = WebEngine(config=webEngineConfig)
-    webEngine.start()
+    # webEngine = WebEngine(config=webEngineConfig)
+    # webEngine.start()
 
     # Create gundamhobby session and execute
     gundamhobbyCaSession = WwwGundamhobbyCa()
-    gundamhobbySessionData = gundamhobbyCaSession.execute(webEngine)
+    # gundamhobbySessionData = gundamhobbyCaSession.execute(webEngine)
+
+    # Use json cache instead of executing for 10min
+    with open('./2022-01-16.json.log') as f:
+        gundamhobbySessionDataJson = json.load(f)
+    gundamhobbySessionData = {}
+    for category, itemsRaw in gundamhobbySessionDataJson.items():
+        gundamhobbySessionData[category] = []
+        for itemRaw in itemsRaw:
+            gundamhobbySessionData[category].append(Item(1,itemRaw['name'],itemRaw['category']))
 
     # Create DBEngine and save gundamhobby results
     db = DBEngine()
