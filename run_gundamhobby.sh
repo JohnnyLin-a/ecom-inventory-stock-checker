b4e59a24911ac5fb1295ba9734291ae54902fa72
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-01-19 00:10:53 -0500

    Add detached. rm and name flag for run_gundamhobby

--- run_gundamhobby.sh
+++ run_gundamhobby.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -w /src -v $(pwd):/src --network=host ecom-inventory-stock-checker:latest bash -c "python3 -m cmd.gundamhobby"
+docker run --rm -d --name "run_gundamhobbby" -w /src -v $(pwd):/src --network=host ecom-inventory-stock-checker:latest bash -c "python3 -m cmd.gundamhobby"
