c0afa9d504ec1d683ada573629bd11b319874141
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-01-23 17:05:02 -0500

    Add niigs to run script

--- run_gundamhobby.sh
+++ run_gundamhobby.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -d --name "run_gundamhobbby" -w /src -v $(pwd):/src -e PYTHONUNBUFFERED=1 --network=host ecom-inventory-stock-checker:latest bash -c "python3 -m cmd.gundamhobby"
+docker run --rm -d --name "run_gundamhobbby" -w /src -v $(pwd):/src -e PYTHONUNBUFFERED=1 --network=host ecom-inventory-stock-checker:latest bash -c "python3 -m cmd.niigs; python3 -m cmd.gundamhobby"
