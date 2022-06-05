04cd86fa3d24a9938cd17491652c2dc3acb44a1b
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-05 04:15:51 -0400

    Add discord chunk processor to post content

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 /**
  * Main entrypoint for ecom-inventory-stock-checker
  */
+import axios from "axios"
+import { setTimeout } from "timers/promises"
 import Ecom from "./internal/ecom/Ecom"
 import EcomConfig from "./internal/ecom/EcomConfig"
 import EcomMap from "./internal/ecom/EcomMap"
@@ -12,21 +14,56 @@ const _ecomConfigs = process.env.ECOM_CONFIG
 
 const ecomConfigs = _ecomConfigs as EcomConfig[]
 const ecomImpls: Ecom[] = []
+let ecomComplete = 0
+interface iDiscordChunks {
+    webhook: string
+    content: string
+}
+const discordChunks: iDiscordChunks[] = []
 
-console.log("Detecting stores...")
-ecomConfigs.forEach((config) => {
-    if (typeof EcomMap[config.url] !== "undefined") {
-        console.log("Found", config.url)
-        ecomImpls.push(new EcomMap[config.url](config))
+// Start of execution below
+
+;(async () => {
+    console.log("Detecting stores...")
+    ecomConfigs.forEach((config) => {
+        if (typeof EcomMap[config.url] !== "undefined") {
+            console.log("Found", config.url)
+            ecomImpls.push(new EcomMap[config.url](config))
+        }
+    })
+
+    ecomImpls.forEach((val) => {
+        val.execute()
+            .then(async (items: Item[]) => {
+                // Save execution data to db
+                const saveDataResult = await val.saveData(items)
+
+                // Send to discord if save is successful
+
+                // Compare diff against previous run & get full inventory
+
+                // Send to discord in chunk
+            })
+            .catch((reason) => {
+                console.log(reason)
+            })
+            .finally(() => {
+                ecomComplete++
+            })
+    })
+
+    // Big loop to finish off the discord chunks as long as the ecom scrape has not finished yet, or when there are still chunks to send
+    while (ecomComplete !== ecomImpls.length || discordChunks.length !== 0) {
+        const reqData = discordChunks.shift()
+        if (reqData) {
+            const response = await axios.post(reqData.webhook, {
+                content: reqData.content,
+            })
+            if (response.status < 200 || response.status >= 300) {
+                console.log("Failed to send content to", reqData.webhook)
+            }
+            await setTimeout(3000) // 3s is the buffer time for discord
+        }
+        await setTimeout(1) // Prevents cpu from being pinned at 100%
     }
-})
-
-ecomImpls.forEach((val) => {
-    val.execute()
-        .then((items: Item[]) => {
-            console.log(JSON.stringify(items))
-        })
-        .catch((reason) => {
-            console.log(reason)
-        })
-})
+})()
