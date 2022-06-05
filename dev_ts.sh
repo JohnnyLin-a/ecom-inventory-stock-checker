0216e80831fb506a17b3eb28a821f5528c84edf7
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-05 20:07:04 +0000

    Add network mode host to dev

--- dev_ts.sh
+++ dev_ts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v $PWD:/src -w /src --env-file postgres.env node:18-alpine sh
+docker run --rm -it -v $PWD:/src -w /src --env-file postgres.env --network host node:18-alpine sh
