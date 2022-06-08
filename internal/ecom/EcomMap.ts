a738948990ae452490eece09d021af1d45166018
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-08 05:00:59 +0000

    Add gundamhangar.com

--- internal/ecom/EcomMap.ts
+++ internal/ecom/EcomMap.ts
@@ -1,3 +1,4 @@
+import GundamhangarCom from "./GundamhangarCom"
 import GundamhobbyCa from "./GundamhobbyCa"
 import NiigsCa from "./NiigsCa"
 import ScifianimeCa from "./ScifianimeCa"
@@ -6,5 +7,6 @@ const map: any = {
     "https://www.gundamhobby.ca": GundamhobbyCa,
     "https://niigs.ca": NiigsCa,
     "https://scifianime.ca": ScifianimeCa,
+    "https://server.gundamhangar.com": GundamhangarCom,
 }
 export default map
---
+++ internal/ecom/GundamhangarCom.ts
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { setTimeout } from "timers/promises"
+import Ecom from "./Ecom"
+import Item from "./Item"
+import { REQUEST_HEADER } from "./EcomConfig"
+
+class GundamhangarCom extends Ecom {
+    async execute(): Promise<Item[]> {
+        console.log(this.config.url, "execute")
+        const items: Item[] = []
+        let page = 0
+        let maxPage = 0
+        while (page == 0 || page < maxPage) {
+            console.log(this.config.url, ++page, "/", maxPage)
+            let nextUrl: string | undefined =
+                this.config.url +
+                `/api/products?category=&featured=&groupproducts=&gundambaseexclusive=&limit=100&new=&outofstock=&page=${page}&pbandai=&preorder=&rare=&series=&sort=&special=&thirdparty=&thirdpartyname=`
+            let response = await axios.get(nextUrl, {
+                headers: REQUEST_HEADER,
+            })
+            if (response.status < 200 || response.status >= 300) {
+                return Promise.reject(response.status)
+            }
+            await setTimeout(1000)
+            if (maxPage == 0) {
+                maxPage = response.data.pagination.total
+            }
+
+            response.data.data.forEach((item: any) => {
+                if (item.stock != "0") {
+                    items.push(new Item(1, item.title))
+                }
+            })
+            
+        }
+
+        return Promise.resolve(items)
+    }
+}
+
+export default GundamhangarCom
