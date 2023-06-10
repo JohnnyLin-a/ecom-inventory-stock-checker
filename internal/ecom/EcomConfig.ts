34898f11b956016a4aaf9a6885aee2cc1d1221b5
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2023-06-10 21:31:56 +0000

    Update user-agent

--- internal/ecom/EcomConfig.ts
+++ internal/ecom/EcomConfig.ts
@@ -9,5 +9,5 @@ export default EcomConfig
 
 export const REQUEST_HEADER = {
     "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
 }
