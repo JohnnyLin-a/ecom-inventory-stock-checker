873580fd25aa08df9271867a5471330c780588ea
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2023-06-10 21:34:48 +0000

    Fix ecom list

--- internal/ecom/EcomMap.ts
+++ internal/ecom/EcomMap.ts
@@ -3,10 +3,17 @@ import ArgamahobbyCom from "./ArgamahobbyCom"
 import GundamhangarCom from "./GundamhangarCom"
 import GundamhobbyCa from "./GundamhobbyCa"
 import NiigsCa from "./NiigsCa"
-import ScifianimeCa from "./ScifianimeCa"
+// import ScifianimeCa from "./ScifianimeCa" Store is now closed :(
 import AnimeExtreme from "./AnimeExtreme"
+import Ecom from "./Ecom"
+import EcomConfig from "./EcomConfig"
 
-const map: any = {
+const map: { [key: string]: new (config: EcomConfig) => Ecom } = {
     "https://animextreme.ca": AnimeExtreme,
+    "https://server.gundamhangar.com": GundamhangarCom,
+    "https://niigs.ca": NiigsCa,
+    "https://www.gundamhobby.ca": GundamhobbyCa,
+    "https://www.agesthreeandup.ca": AgesThreeAndUpCa,
+    "https://argamahobby.com": ArgamahobbyCom,
 }
 export default map
--- setup.sql
+++ setup.sql
@@ -1,33 +1,35 @@
 -- This file is used to setup the database from scratch
-
 CREATE TABLE ecoms (
     id BIGSERIAL PRIMARY KEY,
     website varchar(50) NOT NULL UNIQUE
 );
-
 CREATE TABLE executions (
     id BIGSERIAL PRIMARY KEY,
     ecom_id BIGINT NOT NULL REFERENCES ecoms(id) ON DELETE CASCADE,
     exec_datetime timestamptz NOT NULL DEFAULT NOW(),
     successful BOOLEAN NOT NULL DEFAULT FALSE
 );
-
 CREATE TABLE items (
     id BIGSERIAL PRIMARY KEY,
     ecom_id BIGINT NOT NULL REFERENCES ecoms(id) ON DELETE CASCADE,
     name varchar(255) NOT NULL
 );
-
 CREATE TABLE execution_item_stocks (
     execution_id BIGINT NOT NULL REFERENCES executions(id) ON DELETE CASCADE,
     item_id BIGINT NOT NULL REFERENCES items(id) ON DELETE CASCADE,
     stock INT NOT NULL DEFAULT 1
 );
-
+INSERT INTO ecoms (website)
+VALUES ('https://animextreme.ca'),
+    ('https://server.gundamhangar.com'),
+    ('https://niigs.ca'),
+    ('https://www.gundamhobby.ca'),
+    ('https://www.agesthreeandup.ca'),
+    ('https://argamahobby.com');
 /*
-Drop tables in this order if tearing down
-    op.drop_table('execution_item_stocks')
-    op.drop_table('items')
-    op.drop_table('executions')
-    op.drop_table('ecoms')
-*/
+ Drop tables in this order if tearing down
+ op.drop_table('execution_item_stocks')
+ op.drop_table('items')
+ op.drop_table('executions')
+ op.drop_table('ecoms')
+ */
